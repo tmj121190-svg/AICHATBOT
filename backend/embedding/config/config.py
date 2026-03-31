f74623ec09c8dc244575b435d78c653cebc68263
tmj121190-svg <tmj121190@gmail.com>
2026-03-31 22:10:53 +0200

    Ko lort

---
+++ backend/embedding/config/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+# Load envriment variables from .env file
+load_dotenv()
+
+class Config:
+    
+    # Database configs
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_ENDPOINT = os.getenv("QDRANT_ENDPOINT")
+    
+    # AI MODEL from Hugging face
+    MODEL_NAME = os.getenv("MODEL_NAME")
+
+    # Collection name  / Table name in database
+    COLLECTION_NAME = os.getenv("COLLECTION_NAME")
